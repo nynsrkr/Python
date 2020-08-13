f5bf29fb564dc41518838172305a379ea831faa9
Nayan Sarkar <realmatrix@outlook.com>
2020-08-13 18:53:43 -0400

    test

--- reverse.py
+++ reverse.py
@@ -1,8 +1,1 @@
-number=int(input("Enter the number you want to reverse "))
-reverse=0
-while(number>0):
-    last=number%10
-    reverse=(reverse*10)+last
-    number=number//10
-string=str(reverse)
-print("The reversed number is "+string)
+print("test")
