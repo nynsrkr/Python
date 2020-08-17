eed05a60468fc4eb88fecebe837269f0d3cceb1d
Nayan Sarkar <realmatrix@outlook.com>
2020-08-17 01:52:11 -0400

    sort/reverse, length of a list

--- list-practice.py
+++ list-practice.py
@@ -1,27 +1,18 @@
 #Store a message in a variable, and print that message Then change the value of your variable to a new message, and print the new message
-message="How are you"
-print(message)
-message="This is the new message"
-print(message)
 
 #2-3. Personal Message: Store a person’s name in a variable, and print a message to that person Your message should be simple, such as, “Hello Eric,would you like to learn some Python today?”
-name="nayan"
-print(message+" How are you today "+name)
 
 #2-4. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase
-print(name.title())
-print(name.upper())
 
 #2-7. Stripping Names: Store a person’s name, and include some whitespace
 #characters at the beginning and end of the name Make sure you use each
 #character combination, "\t" and "\n", at least once
-print("\t\nHello World")
 
 #Print the name once, so the whitespace around the name is displayed
 #Then print the name using each of the three stripping functions, lstrip(),
 #rstrip(), and strip()
 
-# create a list , #print list
+#create a list , #print list
 
 #print particular values in a list
 
@@ -46,3 +37,13 @@ print("\t\nHello World")
 #store and print or run methods on specific popped variable
 
 #use the remove() method to work with a value that’s being removed from a list.
+
+#method sort list
+
+#sort list in reverse
+
+#tempory sorting of list
+
+#reverse a list
+
+# find the length of a list
