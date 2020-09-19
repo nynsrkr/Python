209e6ae6b6e8ddab4a17d09c9eaa0c4c8d2741d8
Nayan Sarkar <realmatrix@outlook.com>
2020-09-19 04:18:11 -0400

    edits

--- list-practice-template.py
+++ list-practice-template.py
@@ -1,17 +1,18 @@
 #Store a message in a variable, and print that message Then change the value of your variable to a new message, and print the new message
 
 #2-3. Personal Message: Store a person’s name in a variable, and print a message to that person Your message should be simple, such as, “Hello Eric,would you like to learn some Python today?”
-
+ 
 #2-4. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase
-
+ 
 #2-7. Stripping Names: Store a person’s name, and include some whitespace
 #characters at the beginning and end of the name Make sure you use each
 #character combination, "\t" and "\n", at least once
+ 
 
 #Print the name once, so the whitespace around the name is displayed
 #Then print the name using each of the three stripping functions, lstrip(),
 #rstrip(), and strip()
-
+ 
 #create a list , #print list
 
 #print particular values in a list
@@ -21,18 +22,21 @@
 #print particular variable at reverse index in list
 
 #apply methods such as title,upper,lower, left and right blank strip
+ 
 
 #replace values in list at index
+ 
 
 #add values in list
-
+ 
 #add value in list at index  #REVISE THIS
-
+ 
 #delete values in list
-
+ 
 #remove value in list when position not know read page 78 #REVISE
-
+ 
 #store and print or run methods on popped variables
+
 
 #store and print or run methods on specific popped variable
 
